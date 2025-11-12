f43cb78ead3549b3e7c00cb8f70442856a110642
TlFoxHuman <tlfoxhuman@examle.com>
2025-11-12 23:09:56 +0900

    WIP: Improving Getting back Windows Tools folder method

--- 11Tweaks.ps1
+++ 11Tweaks.ps1
@@ -65,7 +65,7 @@ if ($YESORNO -ne "n" -and $YESORNO -ne "N") {
 
 $YESORNO = Read-Host "Do you want to download Firefox?(Y/n): "
 if ($YESORNO -ne "n" -and $YESORNO -ne "N") {
-    $YESORNO = Read-Host "日本語版をダウンロードしますか? (Do you want to download Japanese version?(y/N) "
+    $YESORNO = Read-Host "Do you want to download Japanese version?(y/N) "
     if ($YESORNO -eq "Y" -or $YESORNO -eq "y") {
         $DLURI = "https://download.mozilla.org/?product=firefox-stub&os=win64&lang=ja"
     } else {
@@ -132,24 +132,42 @@ $YESORNO = Read-Host "Do you want to get back the Windows Tools folder in start 
 if ($YESORNO -ne "n" -and $YESORNO -ne "N") {
     $DESTACCESS="WindowsAccessories"
     $DESTSYSTOOL="SystemTools"
-    $DESTADMTOOL="Administrative Tools"
-    $YESORNO = Read-Host "フォルダ名に日本語を使用しますか? (Do you want to use Japanese for the destination folder name?) (y/N)"
+    $DESTADMTOOL="AdministrativeTools"
+    $YESORNO = Read-Host "Do you want to use Japanese for the destination folder name? (y/N)"
     if ($YESORNO -eq "y" -or $YESORNO -eq "Y") {
         $DESTACCESS="WindowsWindowsアクセサリ"
         $DESTSYSTOOL="Windowsシステムツール"
         $DESTADMTOOL="Windows管理ツール"
     }
+    #Accessories
     New-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTACCESS" -ItemType "Directory" -Force
+    
+    #System Tools
     New-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTSYSTOOL" -ItemType "Directory" -Force
+    New-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTSYSTOOL\desktop.ini" -ItemType "File" -Force
+    [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("W0xvY2FsaXplZEZpbGVOYW1lc10NClRhc2sgTWFuYWdlci5sbms9QCVTeXN0ZW1Sb290JVxzeXN0ZW0zMlxUYXNrbWdyLmV4ZSwtMzI0MjANCkNvbW1hbmQgUHJvbXB0Lmxuaz1AJVN5c3RlbVJvb3QlXHN5c3RlbTMyXHNoZWxsMzIuZGxsLC0yMjAyMg0KUnVuLmxuaz1AJVN5c3RlbVJvb3QlXHN5c3RlbTMyXHNoZWxsMzIuZGxsLC0xMjcxMA0KQ29udHJvbCBQYW5lbC5sbms9QCVTeXN0ZW1Sb290JVxzeXN0ZW0zMlxzaGVsbDMyLmRsbCwtMTI3MTINCg==")) | Out-File "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTSYSTOOL\desktop.ini"
+    Move-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\*.lnk" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTACCESS" -Force
+    Move-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\System Tools\*.lnk" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTSYSTOOL" -Force
+    $fobj = Get-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTSYSTOOL"
+    $fobj.Attributes = "System"
+    $fobj = Get-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTSYSTOOL/desktop.ini"
+    $fobj.Attributes = "System", "Hidden"
+    
+    # Administrative tools
     New-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTADMTOOL" -ItemType "Directory" -Force
+    New-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTADMTOOL\desktop.ini" -ItemType "File" -Force
+    [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("W0xvY2FsaXplZEZpbGVOYW1lc10NCmlTQ1NJIEluaXRpYXRvci5sbms9QCVTeXN0ZW1Sb290JVxzeXN0ZW0zMlxpc2NzaWNwbC5kbGwsLTUwMDENCk9EQkMgRGF0YSBTb3VyY2VzICgzMi1iaXQpLmxuaz1AJVN5c3RlbVJvb3QlXHN5c3dvdzY0XG9kYmNpbnQuZGxsLC0xNjkzDQpPREJDIERhdGEgU291cmNlcyAoNjQtYml0KS5sbms9QCVTeXN0ZW1Sb290JVxzeXN0ZW0zMlxvZGJjaW50LmRsbCwtMTY5NA0KTWVtb3J5IERpYWdub3N0aWNzIFRvb2wubG5rPUAlU3lzdGVtUm9vdCVcc3lzdGVtMzJcTWRTY2hlZC5leGUsLTQwMDENCkV2ZW50IFZpZXdlci5sbms9QCVTeXN0ZW1Sb290JVxzeXN0ZW0zMlxtaWd1aXJlc291cmNlLmRsbCwtMTAxDQpDb21wdXRlciBNYW5hZ2VtZW50Lmxuaz1AJVN5c3RlbVJvb3QlXHN5c3RlbTMyXG15Y29tcHV0LmRsbCwtMzAwDQpDb21wb25lbnQgU2VydmljZXMubG5rPUAlc3lzdGVtcm9vdCVcc3lzdGVtMzJcY29tcmVzLmRsbCwtMzQxMA0Kc2VydmljZXMubG5rPUAlc3lzdGVtcm9vdCVcc3lzdGVtMzJcZmlsZW1nbXQuZGxsLC0yMjA0DQpTeXN0ZW0gQ29uZmlndXJhdGlvbi5sbms9QCVzeXN0ZW1yb290JVxzeXN0ZW0zMlxtc2NvbmZpZy5leGUsLTUwMDYNClN5c3RlbSBJbmZvcm1hdGlvbi5sbms9QCVzeXN0ZW1yb290JVxzeXN0ZW0zMlxtc2luZm8zMi5leGUsLTEwMA0KV2luZG93cyBEZWZlbmRlciBGaXJld2FsbCB3aXRoIEFkdmFuY2VkIFNlY3VyaXR5Lmxuaz1AJVN5c3RlbVJvb3QlXFN5c3RlbTMyXEF1dGhGV0dQLmRsbCwtMjANClRhc2sgU2NoZWR1bGVyLmxuaz1AJVN5c3RlbVJvb3QlXHN5c3RlbTMyXG1pZ3VpcmVzb3VyY2UuZGxsLC0yMDENCkRpc2sgQ2xlYW51cC5sbms9QCVTeXN0ZW1Sb290JVxzeXN0ZW0zMlxzaGVsbDMyLmRsbCwtMjIwMjYNCmRmcmd1aS5sbms9QCVzeXN0ZW1yb290JVxzeXN0ZW0zMlxkZnJndWkuZXhlLC0xMDMNClBlcmZvcm1hbmNlIE1vbml0b3IubG5rPUAlU3lzdGVtUm9vdCVcc3lzdGVtMzJcd2RjLmRsbCwtMTAwMjENClJlc291cmNlIE1vbml0b3IubG5rPUAlU3lzdGVtUm9vdCVcc3lzdGVtMzJcd2RjLmRsbCwtMTAwMzANClJlZ2lzdHJ5IEVkaXRvci5sbms9QCVTeXN0ZW1Sb290JVxyZWdlZGl0LmV4ZSwtMTYNClNlY3VyaXR5IENvbmZpZ3VyYXRpb24gTWFuYWdlbWVudC5sbms9QCVTeXN0ZW1Sb290JVxzeXN0ZW0zMlx3c2VjZWRpdC5kbGwsLTcxOA0KUHJpbnQgTWFuYWdlbWVudC5sbms9QCVzeXN0ZW1yb290JVxzeXN0ZW0zMlxwbWNzbmFwLmRsbCwtNzAwDQpSZWNvdmVyeURyaXZlLmxuaz1AJXN5c3RlbXJvb3QlXHN5c3RlbTMyXFJlY292ZXJ5RHJpdmUuZXhlLC01MDANCg==")) | Out-File "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTADMTOOL\desktop.ini"
+    Move-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Administrative Tools\*.lnk" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTADMTOOL" -Force
+    Move-Item -Path "$HOME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Administrative Tools\*.lnk" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTADMTOOL" -Force
+    $fobj = Get-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTADMTOOL"
+    $fobj.Attributes = "System"
+    $fobj = Get-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTADMTOOL\desktop.ini"
+    $fobj.Attributes = "System", "Hidden"
+    
     # System Folder
-    Move-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\*" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTACCESS" -Force
-    Move-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\System Tools\*" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTSYSTOOL" -Force
-    Move-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Administrative Tools\*" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTADMTOOL" -Force
     # User Folder
     Move-Item -Path "$HOME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Accessories\" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTACCESS" -Force
     Move-Item -Path "$HOME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\System Tools\*" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTSYSTOOL" -Force
-    Move-Item -Path "$HOME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Administrative Tools\*" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$DESTADMTOOL" -Force
 }
 
 # Install ExplorerPatcher
